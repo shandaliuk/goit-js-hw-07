50d93d00a9e9aa1e9c6b7ae8f21d3ff93191739e
Kyrylo Shandaliuk <shandaliuk3019@gmail.com>
2022-12-06 16:38:26 +0200

    Finished second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListElement = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(item => {
+    return `
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`;
+  })
+  .join('');
+
+galleryListElement.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
